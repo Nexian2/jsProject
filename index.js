42edb40af4e0191b9ac62a3d68d94a49059ef1c1
Tsar Luthfi Adrian <156413784+Nexian2@users.noreply.github.com>
2025-11-07 13:45:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// let i = 5
+// while (i < 20) {
+//     i++
+//     console.log('hello')
+// }
+
+const arraySaya = ['apel', 'mangga', 'melon', 'semangka']
+const newArraySaya = arraySaya
+newArraySaya[4] = 'engga'
+
+console.log({ arraySaya})
+console.log({ newArraySaya })
---
+++ index.py
@@ -0,0 +1,8 @@
+nilai = 2
+nama = input("masukkan nilai: ")
+if nilai == nama: {
+    print('anda benar')
+}
+else: {
+    print('anda salah')
+}
